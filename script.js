1a2fdbaf2bf521cf294111cbb05922017887e7c6
Zayan Sheikh <dbzayan@gmail.com>
2024-06-06 06:01:11 -0700

    Trying to add Perlin Noise (and failing) but added a mesh ball too

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-// Sizes
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+
+const uniforms = {
+    u_resolution: {type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight)},
+    u_time: {type: 'f', value: 0.0}
+}
+
+const material = new THREE.ShaderMaterial ({
+    uniforms,
+    vertexShader: document.getElementById('vertexshader').textContent,
+    fragmentShader: document.getElementById('fragmentshader').textContent
+});
 
 
 // Canvas
@@ -16,9 +27,10 @@ const scene = new THREE.Scene();
 
 
 // Material
-const geometry = new THREE.TorusGeometry(0.5, 0.1)
-const material = new THREE.MeshStandardMaterial({ color: 0x5566aa});
+const geometry = new THREE.IcosahedronGeometry(1, 10)
+
 const mesh = new THREE.Mesh(geometry, material);
+mesh.material.wireframe = true;
 
 scene.add(mesh);
 
@@ -39,6 +51,25 @@ scene.add(ambientLight);
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
 directionalLight.position.set(0,0,2);
 scene.add(directionalLight);
+
+// Audio
+const listener = new THREE.AudioListener();
+camera.add(listener);
+
+const sound = new THREE.Audio(listener);
+
+const audioLoader = new THREE.AudioLoader();
+
+audioLoader.load('./media/fillTheVoid.mp3', function(buffer) {
+    sound.setBuffer(buffer);
+    window.addEventListener('click', function() {
+        if (sound.isPlaying) {
+            sound.pause();
+        } else {
+            sound.play();
+        }
+    });
+})
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -66,6 +97,8 @@ function animate() {
 }
 
 renderer.setAnimationLoop(animate);
+
+
 
 // Responsiveness
 window.addEventListener('resize', function() {
